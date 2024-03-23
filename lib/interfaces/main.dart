0c79d0b6f4e8d69ccadfb53a8eb6b7a71ec59f52
Tadeo Diaz <josemanueltadeo2003@gmail.com>
2024-03-23 16:55:03 -0600

    adds(general): Se creo los archivos de las interfaces de usuario y administrador

---
+++ lib/interfaces/main.dart
@@ -0,0 +1,29 @@
+import 'package:aplicacion_ios/interfaces/BNnavigation/button_navbar_user.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Material App', home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        bottomNavigationBar: Navbar(),
+        body: Center(
+          child: Text('Contenido '),
+        ));
+  }
+}
